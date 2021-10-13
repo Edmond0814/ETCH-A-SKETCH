c01f8381dcbd20cbf2e8472bcbaacdc466ade318
Edmond <edmondyong@mobius-digital.com>
2021-10-13 18:03:57 +0800

    last fix

--- script.js
+++ script.js
@@ -16,25 +16,38 @@ sand_box.appendChild(container);
 content.appendChild(sand_box)
 
 const div = document.createElement('div');
-div.classList.add('box')
+div.classList.add('box','sketch')
 
-for (let i=0;i<3**2;i++){
+let box_size = Math.min(document.documentElement.clientWidth/16,(document.documentElement.clientHeight-50)/16)
+container.setAttribute('style',`grid-template-columns: repeat(16,${box_size}px)`)
+
+for (let i=0;i<16**2;i++){
     container.appendChild(div.cloneNode(true))
 }
 
 container.addEventListener('mouseover',function(e){
-    if(e.target.className=='box'){
-        e.target.classList.add('sketch')
+    if(e.target.className=='box sketch'){
+        let opacity = window.getComputedStyle(e.target).opacity + 0.1
+        console.log(window.getComputedStyle(e.target).opacity);
+        e.target.setAttribute('style',`opacity:${opacity}`)
     }
 })
 
 const reset = document.querySelector('.button')
-reset.addEventListener('click', function(){
+reset.addEventListener('click', function sand_boxes(){
     while(container.firstElementChild){
         container.removeChild(container.firstElementChild);
     }
     
-    let user_input = prompt("Number of squares per side?")
+    let user_input=''
+
+    do{
+        user_input = prompt("Number of squares per side?", "maximum of 100")
+        if (user_input>100||user_input<0){
+            user_input=prompt("Please enter a number between 1-100!")
+        }
+    } while(user_input>100||user_input<0)
+
     let box_size = Math.min(document.documentElement.clientWidth/user_input,(document.documentElement.clientHeight-50)/user_input)
     container.setAttribute('style',`grid-template-columns: repeat(${user_input},${box_size}px)`)
 
